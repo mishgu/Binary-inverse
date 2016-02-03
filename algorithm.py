02a9605c2fce994a905f50488933d740535ea43c
Mykhailo Huzyuk <g.and.m@ukr.net>
2016-02-03 11:30:19 +0200

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,53 +1,51 @@
 def binary_inverse (list_bits):
     decimal_value = 0
     i = 0
-    # loop for bit to number
     for el in list_bits:
         decimal_value += el*(-2)**i
         i+=1
-    # if number is not 0
     if decimal_value != 0:
         i = 0
-        # star process: mult bits by -1 
         result_list = [list_bits[0]]
+        
         for el in list_bits[1:]:
             result_list.append(list_bits[i]+list_bits[i+1])
             i+=1
         result_list.append(list_bits[i])
-        # end process !
-        # when we mult we get 2 but bits is 0 or 1
-        # then we delete 2
-        result_list = result_list[::-1] # get reverse because we need go back to front
+        result_list = result_list[::-1]
         i = 0
-        for el in result_list:
-            if (el == 2):
-            	# case when 12* we change 00*
-                if( result_list[i-1] == 1 and (result_list[i+1] == 1 or result_list[i+1] == 2)):
+        while i  != len(result_list):
+            if (result_list[i] == 2):
+                if( result_list[i-1] == 1 and result_list[i+1] == 1):
                     result_list[i-1] = 0
                     result_list[i] = 0
-                # case when *021 we change 1101
+                elif( result_list[i-1] == 1 and result_list[i+1] == 2):
+                    result_list[i-1] = 0
+                    result_list[i] = 0
                 elif( result_list[i-1] == 0 and result_list[i+1] == 1):
-                    #case when * == 1 then we get 2 but bits is 0 or 1
                     if (result_list[i-2] == 1):
                     	result_list[i-2] = 2
                     	result_list[i-1] = 1
                         result_list[i] = 0
                     	i-=2
                     	continue
-                    #case when * == 0 
                     else:
                         result_list[i-2] = 1
                         result_list[i-1] = 1
                         result_list[i] = 0
-                #case when 022 we change 010
                 elif( result_list[i-1] == 0 and result_list[i+1] == 2):
                     result_list[i-1] = 0
                     result_list[i] = 1
-                    result_list[i+1] = 0
-            i+=1    
-        result_list = result_list[::-1] #get normal list
+                    result_list[i+1] = 0    
+            i+=1
+        result_list = result_list[::-1]
         print " Input data\t{0}\n Desimal value\t{1}\n Invale value\t{2}\n Output date\t{3}\n".format(list_bits, decimal_value, decimal_value*(-1),result_list)
-        return result_list
+        decimal_value = 0
+        i=0
+        for el in result_list:
+            decimal_value += el*(-2)**i
+            i+=1
+        print decimal_value
                 
 #test1
 binary_inverse([1, 0, 0, 1])
